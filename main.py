78d0c2de22aa6bc87c72e311fea92ed1971fecb1
Animesh <104458823+animesh27dev@users.noreply.github.com>
2023-06-09 13:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from flask import Flask, request
+from typing import Optional
+from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/get', methods=['GET'])
-def index():
+
+@app.get("/")
+async def root():
     url = request.args.get('url')
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     print(soup.prettify())
     text = soup.get_text()
     return text
-
-if __name__ == '__main__':
-    app.run(debug=True)
